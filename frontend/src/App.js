28cf3b74d5b3ed3aae061d6e1e156026d342caa4
Mateo Clarke <code@mateoclarke.com>
2019-01-30 16:37:24 -0800

    get data from endpoint with axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,39 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: ""
+    };
+  }
+
+  getDataFromEndpoint(endpointUrl) {
+    axios
+      .get(endpointUrl)
+      .then(res => {
+        console.log(res);
+        this.setState({
+          data: res.data
+        });
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
+  }
+
+  componentWillMount() {
+    const socrataApiEndpoint =
+      "https://data.austintexas.gov/resource/yfa7-33gh.json";
+    this.getDataFromEndpoint(socrataApiEndpoint);
+  }
+
   render() {
     return (
       <div className="App">
-        <code>🤠 howdy world!</code>
+        <code>{JSON.stringify(this.state.data)}</code>
       </div>
     );
   }
