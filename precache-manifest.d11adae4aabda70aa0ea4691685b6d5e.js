d2daa41ff19b66d3f527c3348a77ca5738e1c94d
Mateo Clarke <code@mateoclarke.com>
2019-01-31 16:01:51 -0800

    Updates

---
+++ precache-manifest.d11adae4aabda70aa0ea4691685b6d5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "13e594c27eaefe6078c6",
+    "url": "/school-of-data/static/js/runtime~main.13e594c2.js"
+  },
+  {
+    "revision": "710333f0253b1706b814",
+    "url": "/school-of-data/static/js/main.710333f0.chunk.js"
+  },
+  {
+    "revision": "0c2d1c70fb83032f8545",
+    "url": "/school-of-data/static/js/1.0c2d1c70.chunk.js"
+  },
+  {
+    "revision": "710333f0253b1706b814",
+    "url": "/school-of-data/static/css/main.eb09c584.chunk.css"
+  },
+  {
+    "revision": "0c2d1c70fb83032f8545",
+    "url": "/school-of-data/static/css/1.fbc51ee9.chunk.css"
+  },
+  {
+    "revision": "b0c4b4650a4b9519131df546360e845c",
+    "url": "/school-of-data/index.html"
+  }
+];
