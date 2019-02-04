f6f9c2d9d1343110c01faac9f6e5a7183256180d
Mateo Clarke <code@mateoclarke.com>
2019-02-04 10:39:12 -0600

    Add methods to show popup on circle click & row click

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import MaterialTable from "material-table";
-import { Map, TileLayer, Circle, Popup } from "react-leaflet";
+import { Map, TileLayer, Circle, Tooltip } from "react-leaflet";
 import { overrideLeafletIcons } from "./leafletOverrides";
 import "./App.css";
 
@@ -12,7 +12,8 @@ class App extends Component {
     super(props);
     this.state = {
       data: {},
-      isDataLoaded: false
+      isDataLoaded: false,
+      activeTooltip: ""
     };
   }
 
@@ -23,7 +24,10 @@ class App extends Component {
         console.log(res);
         this.setState({
           data: res.data,
-          isDataLoaded: true
+          isDataLoaded: true,
+          position: [30.28, -97.735],
+          zoom: 10,
+          circleRadius: 500
         });
       })
       .catch(error => {
@@ -37,15 +41,33 @@ class App extends Component {
     );
   }
 
+  handleRowClick = (event, rowData) => {
+    console.log(rowData);
+    this.setState({
+      position: [rowData.location_latitude, rowData.location_longitude],
+      zoom: 14,
+      circleRadius: 100,
+      activeTooltip: rowData.request_id
+    });
+  };
+
+  handleCircleClick = (event, circleData) => {
+    console.log(event);
+    console.log(circleData);
+    this.setState({
+      position: [circleData.location_latitude, circleData.location_longitude],
+      zoom: 14,
+      circleRadius: 100,
+      activeTooltip: circleData.request_id
+    });
+  };
+
   render() {
     const columns = [
       { title: "Location Name", field: "location_name" },
       { title: "Type", field: "eval_type" },
       { title: "Request Status", field: "request_status" }
     ];
-
-    const position = [30.28, -97.735];
-    const zoom = 10;
 
     const mapCss = {
       width: "100%",
@@ -76,37 +98,41 @@ class App extends Component {
                 title="Signal Requests"
                 columns={columns}
                 data={this.state.data}
+                onRowClick={this.handleRowClick}
               />
             </div>
             <div className="col map-container">
-              <Map center={position} zoom={zoom} style={mapCss}>
+              <Map
+                center={this.state.position}
+                zoom={this.state.zoom}
+                style={mapCss}
+              >
                 <TileLayer
                   attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                   url="http://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.{ext}"
                   ext="png"
                   subdomains="abcd"
                 />
-                {this.state.data.map((item, i) => {
-                  return (
-                    <Circle
-                      center={[item.location_latitude, item.location_longitude]}
-                      radius={500}
-                      key={`${i}_marker`}
-                      fillColor={circleMarkerStyle[item.eval_type].fillColor}
-                      color={circleMarkerStyle[item.eval_type].color}
-                      weight={circleMarkerStyle[item.eval_type].weight}
-                      fillOpacity={
-                        circleMarkerStyle[item.eval_type].fillOpacity
-                      }
-                    >
-                      <Popup>
+                {this.state.data.map((item, i) => (
+                  <Circle
+                    center={[item.location_latitude, item.location_longitude]}
+                    radius={this.state.circleRadius}
+                    key={`${i}_marker`}
+                    fillColor={circleMarkerStyle[item.eval_type].fillColor}
+                    color={circleMarkerStyle[item.eval_type].color}
+                    weight={circleMarkerStyle[item.eval_type].weight}
+                    fillOpacity={circleMarkerStyle[item.eval_type].fillOpacity}
+                    onClick={e => this.handleCircleClick(e, item)}
+                  >
+                    {this.state.activeTooltip === item.request_id && (
+                      <Tooltip opacity={1} permanent>
                         <h3>{`${item.eval_type} REQUEST`}</h3>
                         <p>{`${item.location_name}`}</p>
                         <p>{`Status ${item.request_status}`}</p>
-                      </Popup>
-                    </Circle>
-                  );
-                })}
+                      </Tooltip>
+                    )}
+                  </Circle>
+                ))}
               </Map>
             </div>
           </div>
